4a4eb9e486865324c7d63c003af2221f09146d4f
TEMIRLAN <97175881+Temirlan1212@users.noreply.github.com>
2022-02-28 08:23:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AuthContextProvider from "./contexts/AuthContext";
+import ProductContextProvider from "./contexts/ProductContext";
+import MainRoutes from "./MainRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <ProductContextProvider>
+            <Navbar />
+            <MainRoutes />
+          </ProductContextProvider>
+        </AuthContextProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductList from "../components/CRUD/ProductList/ProductList";
 
 const ProductsPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
 };
 
 export default ProductsPage;
